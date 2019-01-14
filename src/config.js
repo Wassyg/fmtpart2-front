35f2100682fbdacafa39726da4f3dd721063f160
Wassy Guerro <wguerrouani@gmail.com>
2019-01-14 18:02:33 +0100

    with urlBackend with static IP

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var urlLocal = "http://localhost:3000";
-var urlAws = "http://18.194.131.211:3000";
+var urlAws = "http://3.120.68.209:3000";
 
-var urlBackend = (process.env.NODE_ENV == "development")? urlLocal :urlAws;
+var urlBackend = (process.env.NODE_ENV == "development")? urlLocal : urlAws;
 
 export default urlBackend;
